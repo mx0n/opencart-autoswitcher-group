3101986dc2916583be686fcee2404fbdbfdf4c7e
mx0n <crd.dev@gmail.com>
2018-11-23 20:02:24 +0200

    Добавляет перевод языковых файлов на русский

--- upload/admin/language/russian/sale/customer_switch.php
+++ upload/admin/language/russian/sale/customer_switch.php
@@ -1,11 +1,11 @@
 <?php
 //Added Entry
-$_['entry_title'] = '<b>Customer Group Switcher</b>';
-$_['entry_reach_customer_group'] = 'Switch Customer to Group:';
-$_['entry_reach_value']       = 'When total order reach:';
-$_['entry_reach_order_status']       = 'And order status is:';
-$_['text_group_note']  = 'Customers will be switched to <b>%s</b> group when their <b>%s</b> orders total value reach <b>%s</b> (%s)';
-$_['entry_admin_notify']       = 'Admin Alert Mail:<span class="help">Send email to store owner and additional email when customer group is switched.</span>';
-$_['entry_customer_notify']       = 'Customer Alert Mail:<span class="help">Send email to customer when customer group is switched.</span>';
-$_['entry_s_status']       = 'Enable switch:<span class="help">Check if you want auto switch function is enabled.</span>';
+$_['entry_title'] = '<b>Перевод группы клиентов </b>';
+$_['entry_reach_customer_group'] = 'Перевод клиента в группу:';
+$_['entry_reach_value']       = 'Когда оборот достигнет:';
+$_['entry_reach_order_status']       = 'И статусы заказов:';
+$_['text_group_note']  = 'Клиент будет переведен в группу <b>%s</b> когда <b>%s</b> общая сумма оборота достигнет <b>%s</b> (%s)';
+$_['entry_admin_notify']       = 'Оповещение администратору:<span class="help">Отправить письмо администратору, когда клиент перешел в новую группу.</span>';
+$_['entry_customer_notify']       = 'Оповещение клиента:<span class="help">Отправить письмо клиенту, когда группа будет изменена.</span>';
+$_['entry_s_status']       = 'Включить перевод в группу:<span class="help">Отметьте чекбокс для активации перевода.</span>';
 ?>
--- upload/catalog/language/russian/mail/customer_switch.php
+++ upload/catalog/language/russian/mail/customer_switch.php
@@ -1,12 +1,12 @@
 <?php
 // Text
-$_['text_subject']  = '%s - You are switched to new customer group';
-$_['text_welcome']  = 'Thank you very much for purchasing at %s!';
-$_['text_login']    = 'Your account has been changed and you are switched to new group: %s, you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_services'] = 'Upon switched to new group, you will be able to access different services special offers, discounts.';
-$_['text_thanks']   = 'Thanks,';
+$_['text_subject']  = '%s - Вы перешли в новую группу клиентов';
+$_['text_welcome']  = 'Большое спасибо за покупки в %s!';
+$_['text_login']    = 'Ваша учетная запись была изменена, и вы перешли в новую группу: %s, вы можете войти, используя свой адрес электронной почты и пароль, посетив наш веб-сайт или по следующему адресу:';
+$_['text_services'] = 'После перехода в новую группу вы сможете получить доступ к специальным предложениям и скидкам.';
+$_['text_thanks']   = 'Спасибо,';
 
 //admin
-$_['text_admin_subject']   = '%s - A Customer is switched to group: %s.';
-$_['text_admin_content']   = 'Customer with email address %s is switched to %s group.';
+$_['text_admin_subject']   = '%s - Клиент переведен в группу: %s.';
+$_['text_admin_content']   = 'Клиент с адресом электронной почты %s переключается на группу %s.';
 ?>
