8bccc5c7f2378787b54bc3675f170f3a737b1e40
mx0n <crd.dev@gmail.com>
2018-11-23 19:50:35 +0200

    Добавляет директории с переводами для локализации

---
+++ upload/admin/language/russian/sale/customer_switch.php
@@ -0,0 +1,11 @@
+<?php
+//Added Entry
+$_['entry_title'] = '<b>Customer Group Switcher</b>';
+$_['entry_reach_customer_group'] = 'Switch Customer to Group:';
+$_['entry_reach_value']       = 'When total order reach:';
+$_['entry_reach_order_status']       = 'And order status is:';
+$_['text_group_note']  = 'Customers will be switched to <b>%s</b> group when their <b>%s</b> orders total value reach <b>%s</b> (%s)';
+$_['entry_admin_notify']       = 'Admin Alert Mail:<span class="help">Send email to store owner and additional email when customer group is switched.</span>';
+$_['entry_customer_notify']       = 'Customer Alert Mail:<span class="help">Send email to customer when customer group is switched.</span>';
+$_['entry_s_status']       = 'Enable switch:<span class="help">Check if you want auto switch function is enabled.</span>';
+?>
